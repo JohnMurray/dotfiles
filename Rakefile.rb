e33b4d1eb419bd80db056a25ac33f60a81833cfd
John Murray <john.m.murray786@gmail.com>
2011-12-10 20:17:31 -0500

    Added rakefile for deploying the DotFiles
    Instead of copying my files in and out of the project folder (depending on
    where that may be) I created a rake file with a default task that will
    copy the "dot" files (and folders) to the current user's home directory.

---
+++ Rakefile.rb
@@ -0,0 +1,16 @@
+require 'rake'
+require 'fileutils'
+
+##----
+## Copy all of the configuration file out into the $HOME directory of the
+## current user. (all files/folders starting with a "dot" except ".git/")
+##----
+task :deploy do
+  Dir[".[^.]*"].each do |file|
+    unless file == '.git'
+      FileUtils.cp_r file, Dir.home
+    end
+  end
+end
+
+task :default => [:deploy]
