a7eea890a822b7a50ee49f3a36e3637f1c68619d
John Murray <me@johnmurray.io>
2013-07-22 01:04:51 -0400

    Updated install.py
    Didn't copy over directories, which might be important for
    things like .vim and what have you, so that has been added
    to the installer.

--- install.py
+++ install.py
@@ -7,14 +7,40 @@
 
 import os, shutil
 
-curr_dir = os.path.dirname(os.path.realpath(__file__))
-files = os.walk(curr_dir).next()[2]
-home = os.environ['HOME']
+def main():
+  curr_dir = os.path.dirname(os.path.realpath(__file__))
+  home = os.environ['HOME']
+
+  stuff = os.walk(curr_dir).next()
+  files = stuff[2]
+  dirs  = stuff[1]
+
+  print("copying to " + home + ":")
+  for f in files:
+    if f[0] == '.':
+      dest = home + '/' + f
+      print('   ' + f)
+      shutil.copyfile(f, dest)
+
+  for d in dirs:
+    if d[0] == '.' and d != '.git':
+      dest = home + '/' + d
+      print('   ' + d + '/')
+      copytree(d, dest, True, None)
 
 
-print("copying to " + home + ":")
-for f in files:
-  if f[0] == '.':
-    dest = home + '/' + f
-    print('   ' + f)
-    shutil.copyfile(f, dest)
+def copytree(src, dst, symlinks=False, ignore=None):
+  if not os.path.exists(dst):
+    os.makedirs(dst)
+  for item in os.listdir(src):
+    s = os.path.join(src, item)
+    d = os.path.join(dst, item)
+    if os.path.isdir(s):
+      copytree(s, d, symlinks, ignore)
+    else:
+      print('   ' + s)
+      shutil.copy2(s, d)
+
+
+
+main()
