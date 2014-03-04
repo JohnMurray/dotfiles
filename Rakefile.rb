451e61a0c1d9824eb3736fb1925a66d723634b0e
John Murray <me@johnmurray.io>
2014-03-03 22:01:18 -0500

    Update Rakefile.rb
    Now deploys ./bin and ./contrib. Also sepeated everything out into
    seperate tasks.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,18 +1,58 @@
 require 'rake'
 require 'fileutils'
 
+task :default => [:deploy]
+task :deploy => [:deploy_contrib, :deploy_bin, :deploy_config]
+ 
+
 ##----
 ## Copy all of the configuration file out into the $HOME directory of the
 ## current user. (all files/folders starting with a "dot" except ".git/")
 ##----
-task :deploy do
-  puts "Copying Files"
+task :deploy_config do
+  puts "Copying Config Files"
   Dir[".[^.]*"].each do |file|
-    unless ['.git', 'bin', 'contrib'].include? file
+    unless ['.git', 'contrib'].include? file
       puts "\tMoving #{file}"
       FileUtils.cp_r file, Dir.home
     end
   end
 end
 
-task :default => [:deploy]
+
+##----
+## Copy all of the files in ./bin to $HOME/bin and ensure that the $HOME/bin
+## directory exists.
+##----
+task :deploy_bin do
+  puts "Deploying ./bin Files"
+  FileUtils.mkdir_p(File.join(Dir.home, 'bin'))
+  Dir.chdir('bin') do
+    Dir['[^.]*'].each do |bin_file|
+      next if ['readme.md'].include? bin_file
+      puts "\tMoving #{bin_file} to ~/bin"
+      FileUtils.cp_r bin_file, File.join(Dir.home, 'bin')
+    end
+  end
+end
+
+
+##----
+## Copy all of the files in ./contrib/$PROJECT_NAME to the $HOME dir in the
+## same strucutre that exists below ./contrib/$PROJECT_NAME.
+##----
+task :deploy_contrib do
+  puts "Deploying ./contrib Files"
+  Dir.chdir('contrib') do
+    Dir['*'].each do |dir|
+      next if ['readme.md'].include? dir
+      puts "\t#{dir}"
+      Dir.chdir(dir) do
+        Dir['*'].each do |file|
+          puts "\t\tMoving #{file}"
+          FileUtils.cp_r file, Dir.home
+        end
+      end
+    end
+  end
+end
